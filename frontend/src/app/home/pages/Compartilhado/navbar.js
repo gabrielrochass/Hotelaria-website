e70dc603cb681b38876f69f8f245ebc7e693b7f3
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-07-23 13:45:12 -0300

    feat: apresentação

--- frontend/src/app/home/pages/Compartilhado/navbar.js
+++ frontend/src/app/home/pages/Compartilhado/navbar.js
@@ -17,8 +17,8 @@ const NavBar = () => {
       </div>
       <div className='navbar-right'>
         <ul className='navbar-links'>
-          <li onClick={() => navigate('/search')}>Buscar</li>
           <li onClick={() => navigate('/')}>Home</li>
+          <li onClick={() => navigate('/search')}>Buscar</li>
           <li onClick={() => navigate('/active-promos')}>Promoções Ativas</li>
           <li onClick={() => navigate('/my-accommodations')}>Minhas Acomodações</li>
           <li onClick={() => navigate('/payment-methods')}>Métodos de pagamento</li>
